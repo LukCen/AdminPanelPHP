df1bc261503e662deb3d85edd7c2e5874a6bd375
lbjcena <lbjcena@gmail.com>
2026-01-02 01:31:40 +0100

    visual improvements, cleaned up the styling, added some subtle animations on user interaction

--- src/GameCard.php
+++ src/GameCard.php
@@ -3,18 +3,11 @@
 
 ?>
 
-<a href='?view=game_page&game=<?= $game['slug'] ?>' class="card flex flex-col gap-2 items-center p-2 bg-secondary">
+<a href='?view=game_page&game=<?= $game['slug'] ?>' class="card flex flex-col gap-2 max-w-1/5 flex-1/5 items-center rounded-xl p-2 bg-secondary">
   <img height="200" width="200" src="<?= htmlspecialchars($game['background_image'] ?? 'https://placehold.co/300x300') ?>" alt="">
-  <div class="text flex flex-col gap-2">
+  <div class="text flex flex-col flex-auto justify-between gap-2">
     <p>Name: <?= htmlspecialchars($game['name']) ?></p>
     <p>Date of release: <?= htmlspecialchars($game['released']) ?></p>
     <button class="bg-light px-2 py-1 w-fit font-bold">Go to game page &rarr; </button>
   </div>
 </a>
-
-<style>
-  .card {
-    max-width: 20%;
-    flex: 1 1 20%;
-  }
-</style>
--- src/Navbar.php
+++ src/Navbar.php
@@ -5,26 +5,29 @@ $views = [
   'developers' => ['label' => 'Developers', 'page_size' => 10]
 ];
 
+
 $active_view = $_GET['view'] ?? 'games'
   ?>
 
 <nav class="flex flex-col gap-2 p-2 bg-secondary">
   <h1>Admin panel</h1>
-  <div class="input-group flex flex-col gap-1">
-    <form method="GET">
-      <label for="searchbar">Search games</label>
+  <div class="input-group search bg-secondary flex flex-col gap-1">
+    <form method="GET" class="flex justify-between flex-auto gap-1">
+      <!-- <label for="searchbar">Search games</label> -->
       <!-- passes currently active view as a parameter to the url - makes sure page reload returns you to correct view post-reload -->
       <input type="hidden" name="view" value=<?= $active_view ?>>
-      <input type="search" name="search" id="searchbar">
-      <button type="submit" class="bg-primary font-bold px-4 py-2">Search</button>
+      <input placeholder="Search for games" type="search" class="flex flex-auto bg-primary px-2 py-2" name="search" id="searchbar">
+      <button type="submit" class="bg-primary flex flex-1/5 font-bold px-4 py-2">Search</button>
     </form>
   </div>
-  <ul class="flex flex-col gap-1">
+  <ul class="nav-menu flex flex-col gap-4">
     <h2>Browse by: </h2>
-    <?php
-    foreach ($views as $view => $param):
-      echo "<li><a href='?view={$view}&page_size={$param["page_size"]}'>" . ucfirst($view) . "</a></li>";
-    endforeach;
-    ?>
+    <?php foreach ($views as $view => $param): ?>
+      <li class="bg-primary px-4 py-2 rounded w-fit <?= $view === $active_view ? 'active' : '' ?>">
+        <a class="block" href="?view=<?= $view ?>&page_size=<?= $param['page_size'] ?>">
+          <?= ucfirst($view) ?>
+        </a>
+      </li>
+    <?php endforeach; ?>
   </ul>
 </nav>
